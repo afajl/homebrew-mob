af621455432b98f54142c4469aef48ac8012a6b3
afajl <p@afajl.com>
2021-09-26 16:46:30 +0200

    mob 1.0.0

--- Formula/mob.rb
+++ Formula/mob.rb
@@ -1,9 +1,9 @@
 class Mob < Formula
   desc "Console tool for streamlining remote mobbing"
   homepage "https://github.com/afajl/mob"
-  url "https://github.com/afajl/mob/releases/download/v1.0.0-beta.1/mob-v1.0.0-beta.1-macos-x86_64-apple-darwin.tar.gz"
-  sha256 "8532247868d7292d657d3a3eebc68d9a33571faf4e6f3a268546f669ac328c34"
-  version "1.0.0-beta.1"
+  url "https://github.com/afajl/mob/releases/download/v1.0.0/mob-v1.0.0-macos-x86_64-apple-darwin.tar.gz"
+  sha256 "066be5966f6a0b7124e66f7905262a22cafc5f88872a78ff56409e77c37d33fe"
+  version "1.0.0"
 
   def install
     bin.install "mob"
