018ab86c1acaded42e237ae75f384f9575aeec0d
Paul Diaconescu <paul.diaconescu@svt.se>
2021-09-26 17:17:48 +0200

    Fix force pushed tag

--- Formula/mob.rb
+++ Formula/mob.rb
@@ -2,7 +2,7 @@ class Mob < Formula
   desc "Console tool for streamlining remote mobbing"
   homepage "https://github.com/afajl/mob"
   url "https://github.com/afajl/mob/releases/download/v1.0.0/mob-v1.0.0-macos-x86_64-apple-darwin.tar.gz"
-  sha256 "066be5966f6a0b7124e66f7905262a22cafc5f88872a78ff56409e77c37d33fe"
+  sha256 "b8055e78543a1ae6878e70a872cde5850042bede68174454b8eb27399491a9d4"
   version "1.0.0"
 
   def install
